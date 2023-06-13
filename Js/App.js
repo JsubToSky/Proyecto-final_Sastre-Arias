ce957f5639c90134a61b1e199d924cc55b00f388
Jasub_Sastre <davidjasubsastrearias@gmail.com>
2023-06-13 11:21:07 -0500

    third-commit

--- Js/App.js
+++ Js/App.js
@@ -1,8 +1,32 @@
+const contenedorCelulares = document.querySelector('#contenedor-celulares');
 
 
 
 
 
+function mostrarCelulares (data){
+    data.forEach(element => {
+        const cardCelular = document.createElement('div');
+        cardCelular.setAttribute('id', `tarjeta-${element?.marca}`);
+        cardCelular.setAttribute('class', 'card');
+        cardCelular.innerHTML = `     
+                                <img src="${element?.img}" class="card-img-top" alt="${element?.descripcion}">
+                                <div class="card-body">
+                                  <h5 class="card-title">Celular ${element?.marca}</h5>
+                                  <p class="card-text">Este celular es muy bueno</p>
+                                  <a href="#" id="btn-agregar-${element.id}" class="btn btn-primary btn-agregar">Agregar al carro</a>
+                                </div>
+                                `
+    });
+    const btnAgregar = document.querySelectorAll('.btn-agregar');
+    btnAgregar.forEach(elem =>{
+        elem.addEventListener('click', (e)=>{
+            agregarAlCarrito(e.target.id);
+        });
+    });
+}
+
+mostrarCelulares(arrayCelulares);
 
 
 
@@ -49,5 +73,5 @@ function actualizarCarrito(){
         total = precio + cantidad;
     }
     total = Math.round(total);
-    document.getElementsByClassName('total-carrito')[0].innerText = '$' + total; */
-}
+    document.getElementsByClassName('total-carrito')[0].innerText = '$' + total; 
+}*/
--- Js/data.js
+++ Js/data.js
@@ -16,5 +16,7 @@ class Celulares{
 
 const arrayCelulares = [];
 arrayCelulares.push(new Celulares("1",399,"Xiaomi",`https://png.pngtree.com/png-vector/20230329/ourmid/pngtree-cute-white-dog-png-image_6656184.png`,`Este es Xiaomi REDMI note 10 \n Tiene muy buena batería`));
-arrayCelularer.push(new Celulares("2",999,"Iphone","../Assets/Cel-Iphone-14ProMax.webp",`Iphone 14 Pro Max, un celular excepcional`));
-arrayCelulares.push(new Celulares("3",499,"Samsung",`../Assets/Cel-Samsung-A03S.png`,`Samsung A03S, una batería muy buena`));
+arrayCelulares.push(new Celulares("2",999,"Iphone","",`Iphone 14 Pro Max, un celular excepcional`));
+arrayCelulares.push(new Celulares("3",499,"Samsung",``,`Samsung A03S, una batería muy buena`));
+
+console.log(arrayCelulares);
